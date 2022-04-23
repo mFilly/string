a622908da62e33b183e8ca56442210b5108f4947
Maria Filimonenko <filimonenkom@gmail.com>
2022-04-23 11:17:41 +0800

    helpers.js changed as a hw task

---
+++ js/helpers.js
@@ -0,0 +1,17 @@
+/* helpers.js */
+
+/** @param {string} name */
+export function getNumberOfChars(name) {
+    return name.length// your code
+  };
+  
+  
+  /** @param {string} name */
+  export function getLower(name) {    
+    return name.toLowerCase()// your code
+  };
+  
+  /** @param {string} name */
+  export function getUpper(name) {    
+    return name.toUpperCase()// your code
+  };
---
+++ js/index.js
@@ -0,0 +1,15 @@
+/* index.js */
+
+import {getNumberOfChars, getLower, getUpper} from "/js/helpers.js";
+
+let answer1 = document.querySelector("#answer1");
+let answer2 = document.querySelector("#answer2");
+let answer3 = document.querySelector("#answer3");
+
+let name = document.querySelector("#your-name");
+
+name.addEventListener("keyup", () => {
+  answer1.textContent = getNumberOfChars(name.value);
+  answer2.textContent = getLower(name.value);
+  answer3.textContent = getUpper(name.value);
+});
